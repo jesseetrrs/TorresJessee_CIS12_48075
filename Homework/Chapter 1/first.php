322cd8227634fe5de55f952b5f019355c59afe16
jesseetrrs <jessicatorres@jessica-torress-macbook-pro.local>
2014-09-05 16:31:24 -0700

    Chapter 1 HW
    first.php

---
+++ Homework/Chapter 1/first.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Basic PHP Page</title>
+</head>
+
+<body>
+<!--Script 1.2- first php -->
+<p> This is standard HTML.</p>
+<?php
+?>
+</body>
+</html>
