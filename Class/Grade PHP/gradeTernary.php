7abe507e579b8912eb15af1121ef6517d1953258
jesseetrrs <jesseetrrs@gmail.com>
2014-09-08 09:14:27 -0700

    Signed-off-by: jesseetrrs <jesseetrrs@gmail.com>

--- Class/Grade PHP/gradeTernary.php
+++ Class/Grade PHP/gradeTernary.php
@@ -21,10 +21,8 @@
 			($score>=80)?'B':(
 			($score>=70)?'C':(
 			($score>=60)?'D':'F')));
-			
-		
-	
-	//Output the Results
+		//Paranthesis needed because it should be written as one line of code notice semicoloncat end of F
+		//Output the Results
 	echo "<h1>A score of $score = $grade</h1>";
 	
 ?>
