45738f2f7a6af52a6d2c53a584a4a665ac2f304a
jesseetrrs <jessicatorres@jessica-torress-macbook-pro.local>
2014-09-08 19:08:19 -0700

    strings

---
+++ Homework/Chapter 1/strings.php
@@ -0,0 +1,28 @@
+<?php
+	/*
+		Jessee Torres
+		Chapter 1 Homework Snippet
+	*/
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Strings</title>
+</head>
+
+<body>
+<?php #Script 1.6-strings.php
+
+$first_name = 'Haruki';
+$last_name = 'Murakami';
+$book = 'Kafka on the Shore';
+
+//You can either say $firstName or $first_name
+echo "<p> The book <em>$book</em>
+	was written by $first_name
+	$last_name. </p>";
+	?>
+</body>
+</html>
