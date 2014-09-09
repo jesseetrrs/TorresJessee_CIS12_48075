0eb12e35a76a210693904f3eb60ab12fdca3ac0f
jesseetrrs <jessicatorres@jessica-torress-macbook-pro.local>
2014-09-08 19:19:40 -0700

    concat

---
+++ Homework/Chapter 1/concat.php
@@ -0,0 +1,32 @@
+<?php
+	/*
+		Jessee Torres
+		Chapter 1 Homework Snippet
+	*/
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Strings</title>
+</head>
+
+<body>
+<?php #Script 1.6-strings.php
+
+$first_name = 'Melissa';
+$last_name = 'Bank';
+$author = $first_name.' ' .
+$last_name;
+
+$book = 'The Girls\' Guide to Hunting
+anf Fishing';
+
+//Print the values:
+echo "<p>The book <em>$book</em>
+was written by $author.</p>";
+ 
+?>
+</body>
+</html>
