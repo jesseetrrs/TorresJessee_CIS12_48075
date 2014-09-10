9227c2ca20d076739a2810c52ff1b135d3ccf2da
jesseetrrs <jesseetrrs@gmail.com>
2014-09-10 09:19:43 -0700

    Signed-off-by: jesseetrrs <jesseetrrs@gmail.com>

--- Class/Grade PHP/InputGrade.php
+++ Class/Grade PHP/InputGrade.php
@@ -15,13 +15,12 @@
 <body>
 <?php
 	//Utilize the built in capabilities of PHP to grab information passed from a form
-	$score=$_GET['score');
+	$score=$_GET['score'];
 	//Determine the grade
 	$grade=($score>=90)?'A':(
 			($score>=80)?'B':(
 			($score>=70)?'C':(
 			($score>=60)?'D':'F')));
-//Paranthesis needed because it should be written as one line of code 			notice semicoloncat end of F. Only need paranethsis in PHP, but it will work in other languages. So keep and impliment for synchronisisty. 
 //Output the Results
 echo "<h1>A score of $score = $grade</h1>";
 	
