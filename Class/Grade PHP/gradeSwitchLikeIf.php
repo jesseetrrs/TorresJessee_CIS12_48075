a2bc9c288761d3efc9d39d3672b7fd8730c832db
jesseetrrs <jesseetrrs@gmail.com>
2014-09-10 08:41:18 -0700

    Signed-off-by: jesseetrrs <jesseetrrs@gmail.com>

---
+++ Class/Grade PHP/gradeSwitchLikeIf.php
@@ -0,0 +1,40 @@
+<?php
+	/* 
+		Jessee Torres
+		Sept. 8th 2014
+		Purpose: Illustrate Branching Constructs
+	*/
+	?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Grade Switch</title>
+</head>
+<body>
+<?php
+	//Initliaze the input
+	$score=rand(50,100);
+	//Determine the grade
+	switch($score>=90){
+		case true:$grade='A'; 
+		default:
+	}
+	switch ($score>=80&&$score<90){
+			case true:$grade='B'; 
+			default:
+	}
+	switch ($score>=70&&$score<80) {
+			case true: $grade='C'; 
+			default:
+	}
+	switch ($score>=60&&$score<70) {
+			case true: $grade='D'; break;
+			default:$grade='F';
+	}
+	
+//Output the Results
+echo "<h1>A score of $score = $grade</h1>";
+?>
+</body>
+</html>
